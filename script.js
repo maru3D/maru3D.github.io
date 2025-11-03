832321dd29407f9d1da17ca4d8b485b816d2d0e7
maru3D <maru3D@proton.me>
2025-11-03 15:59:27 +0100

    Change navigation and update images

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const videos = document.querySelectorAll("video");
-  videos.forEach((video) => {
-    video.addEventListener("mouseover", video.play);
-    video.addEventListener("mouseout", video.pause);
-  });
-});
